461e371840e6cd4b6b7fad3a97b41259fddadb5f
uclhenry <yuhangxuhenry@gmail.com>
2017-12-09 01:00:56 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
     // DB connection info
     //TODO: Update the values for $host, $user, $pwd, and $db
     //using the values you retrieved earlier from the Azure Portal.
-    $host = "blogster.database.windows.net";
+    $host = "blogster.database.windows.net,1433";
     $user = "uclhenry";
     $pwd = "QWEasd123";
     $db = "blogster";
